ae0f7fac36ab5fadf2b0abb5b7ba97f5417e9bfc
Aryans <quispeloayza18@hotmail.com>
2024-01-24 21:40:54 -0500

    Definiciones de las tablas para la migracion

---
+++ speaksmarter/database/migrations/2024_01_25_014648_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',150);
+            $table->text('description')->max(400);
+            $table->string('image_uri', 255)->nullable();
+            $table->string('content_uri',255);
+            $table->string('pdf_uri',255);
+            $table->unsignedInteger('level_id');
+            $table->timestamps();
+
+            //Declaracion de clave foranea a la tabla referencia levels
+            $table->foreign('level_id')
+            ->references('id')->on('levels')
+            ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
