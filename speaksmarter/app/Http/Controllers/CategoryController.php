6b6d248685c126dafa6fd80f092435751f1cc637
Aryans <quispeloayza18@hotmail.com>
2024-01-26 18:51:33 -0500

    Vista para la creacion de categorias

--- speaksmarter/app/Http/Controllers/CategoryController.php
+++ speaksmarter/app/Http/Controllers/CategoryController.php
@@ -27,7 +27,7 @@ class CategoryController extends Controller
     public function create()
     {
         //
-        return "hola";
+        return inertia('Categories/create');
     }
 
     /**
---
+++ speaksmarter/resources/js/Pages/Categories/Create.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { useForm } from '@inertiajs/vue3'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import CategoryForm from '@/Components/Category/Form.vue'
+
+    const form=useForm({
+        name=''
+    })
+</script>
+<template>
+
+    <AppLayout>
+        <template #header>
+            <h1 class="font-semibold text-xl text-gray-800 leading-tight">Create Category</h1>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm-rounded-lg">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            <CategoryForm :form="form" @submit="form.post(route('categories.store'))"></CategoryForm>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+
+</template>
