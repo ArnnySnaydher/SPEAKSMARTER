3919e98d771b057da53972c8e0e9e742d46ad8a7
Aryans <quispeloayza18@hotmail.com>
2024-02-01 17:43:15 -0500

    agregar campo a mi tabla

--- speaksmarter/database/migrations/2024_01_25_016648_create_lessons_table.php
+++ speaksmarter/database/migrations/2024_01_25_016648_create_lessons_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image_uri', 255)->nullable();
             $table->string('content_uri',255);
             $table->string('pdf_uri',255);
+            $table->boolean('is_free')->default(false);
             $table->unsignedBigInteger('level_id');
             $table->timestamps();
 
