f22c2f950f454296ecd3d50883e104f4572074eb
Aryans <quispeloayza18@hotmail.com>
2024-02-01 17:43:40 -0500

    Recorrido para mis checkbox con data de la bd

---
+++ speaksmarter/resources/js/Components/Common/CollectionSelector.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import  {ref} from 'vue';
+    defineProps({
+        collection:{
+            typeof:Array,
+            required:true
+        }
+    })
+    const currentSelection=ref([1])
+    const selection=ref([])
+    const handleAddToSelection=()=>{
+        selection.value.push(currentSelection)
+    }
+
+</script>
+
+<template>
+    <select v-model="currentSelection">
+        <option v-for="(item,index) in collection" :key="index">
+            {{item?.name}}
+        </option>
+    </select>
+    <button @click="handleAddToSelection">Add</button>
+    <div>
+        <ul>
+            <li v-for="(item,inde) in selection" :key="index">
+                {{item}}
+            </li>
+        </ul>
+    </div>
+</template>
