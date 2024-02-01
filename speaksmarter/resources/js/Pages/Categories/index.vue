786b711e4f234a76ada28f821bf331875591810d
Aryans <quispeloayza18@hotmail.com>
2024-02-01 17:42:31 -0500

    cambios en el index de categories

--- speaksmarter/resources/js/Pages/Categories/index.vue
+++ speaksmarter/resources/js/Pages/Categories/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue'
 import { Link } from '@inertiajs/vue3'
+import {Inertia} from  "@inertiajs/inertia";
 
 defineProps({
     categories:{
@@ -25,7 +26,9 @@ const deleteCategory=(id)=>{
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-between" v-if="$page.props.user.permissions.includes('create categories')">
-                        <Link :href="route('categories.create')" class="text-white bg-indigo-500 hover:bg-indigo-700 py-2 px-4 rounded ">CREATE CATEGORIES</Link>
+                        <Link :href="route('categories.create')" class="text-white bg-indigo-500 hover:bg-indigo-700 py-2 px-4 rounded ">
+                            CREATE CATEGORIES
+                        </Link>
                         
                     </div>
 
@@ -41,12 +44,25 @@ const deleteCategory=(id)=>{
                             </div>
                             <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
                             <p class="text-md leading-6 text-gray-900">
-                                <Link :href="route('categories.edit',category.id) " v-if="$page.props.user.permissions.includes('create categories')" >Edit</Link>
-                                <Link @click="deleteCategory(category.id)" v-if="$page.props.user.permissions.includes('create categories')">Delete</Link>
+                                <Link class="py-2 px-4" :href="route('categories.edit',category.id) " v-if="$page.props.user.permissions.includes('update categories')" >Edit</Link>
+                                <Link class="py-2 px-4 text-red-400" @click="deleteCategory(category.id)" v-if="$page.props.user.permissions.includes('delete categories')">Delete</Link>
                             </p>
                             </div>
                         </li>
                     </ul>
+                </div>
+                <div class="flex justify-between mt-2">
+                    
+                        <Link v-if="categories.current_page > 1" :href="categories.prev_page_url" class="py-2 px-4 rounded ">
+                            PREV
+                        </Link>
+                        <div v-else></div>
+                        <Link  v-if="categories.current_page < categories.last_page" :href="categories.next_page_url" class=" py-2 px-4 rounded ">
+                            NEXT
+                        </Link>
+                        <div v-else></div>
+                        
+                        
                 </div>
 
             </div>
